d9273d6ec5b98e1edd72cf44fd63cd1d7886e19f
theonur721 <theonur721@gmail.com>
2024-01-06 21:58:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+
+<h1> YOUTUBE CLONE </h1>
+
+youtubeClone-react
+
+# Kütüphaneler
+
+- react-icons
+- react-router-dom
+- tailwind (kurulumu kendi sayfasında)
+- axios
+- millify
+
+# Yapılacaklar
+
+- `SideNav.jsx` bileşeninde kategorileri listele
+- - Seçilen kategori bilgisini bütün bileşenlerin erişebilmesi için Context yapısında tut
+
+- Seçilen Kategori state'i her değiştiğinde ilgili kategoriye ait youtube video verisini çek
+- Bu veriyi `Feed.jsx` ' kullan ve herbir video objesi için ekrana `VideoCard.jsx` bas
+
+- Videolardan birine tıklandığında kullanıyı o videonun id'sini parametre olrak içeren bir linke yöndlendir
+- - `VideoDetail.jsx` sayfasında urlden parametreyi al
+- - parametreyle braber apiye video detayları için istek at
+- - api'den gelen veriyi ekrana bas
+
+- Eski Yönlendirğimiz Link
+- - parametre yöntemiyle ekledik:
+    /results/react.js
+- - Bu şekilde yönlendirince özel karakterlerde sorun oluyor
+
+- Bu Yüzden Url parametresi yöntemiyle yönlendirme yapacağız :
+- - arama terimi yöntemiyle:
+    /results/?search_query=react.js
+
+
+<h2> Used technologies </h2>
+
+REACT
+
+<h2> Screen Gif </h2>
+
+![](ekran.gif)
+
+# youtubeClone-react
---
+++ src/pages/SearchResults.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { options } from '../utils/constants';
+import SideNav from '../components/SideNav';
+import VideoCard from '../components/VideoCard';
+import loading from '../assets/loading.gif';
+
+const SearchResults = () => {
+  const [videos, setVideos] = useState(null);
+  //   url'den arama terimini alma:
+  const [searchParams, setSearchParams] = useSearchParams();
+  // get methodu yardımıyla aram terimini alma
+  const query = searchParams.get('search_query');
+
+  console.log('ARRAMA TERİMİ>>', query);
+  useEffect(() => {
+    // her aramının başında videolara  null değerini atadık
+    // aşağıdaki sorgu sayesinde null iken loading bastık
+    setVideos(null);
+    // aram termiyle alakalı videoları çekme
+    axios
+      .get(
+        `https://youtube138.p.rapidapi.com/search/?q=${query}`,
+        options
+      )
+      .then((res) => setVideos(res.data.contents));
+  }, [query]);
+
+  console.log(videos);
+
+  return (
+    <div className="flex">
+      <SideNav />
+      <div className="flex justify-center p-5  w-full">
+        {/* videolar yoksa ekran loading basma */}
+        {!videos && (
+          <img className="mx-auto mt-[200px]" src={loading} />
+        )}
+        <div className="flex flex-col gap-20 max-w-[500px]">
+          {videos?.map((content, i) => {
+            if (content.type !== 'video') return;
+            return <VideoCard key={i} videoInfo={content} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
